3d68e0625b906353e3b3f75f9ac68a1089a1ca11
Luis Mantilla <luismantilla99@outlook.com>
2024-06-12 14:38:05 -0400

    update mnist code

--- mnist.py
+++ mnist.py
@@ -1,23 +1,23 @@
-import numpy as np
-
 import torch
 import torchsde
 from torchvision import datasets, transforms
-
-import sys
-
-sys.path.append("./src")
-
 from sklearn.decomposition import PCA
-from springs_sdes import GGS3DE
 
+from src.speed_springs import GGS3DE
 
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x))]
-)
+FRICTION = 10
+TEMP = 1
+K = 1
+M = 1
 
+MAX_TIME=20
+T_SIZE=100
 
-def main(func, ndata_train=100):
+def mnist():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x))]
+    )
+
     trainset = datasets.MNIST(
         root="./data", train=True, download=True, transform=transform
     )
@@ -25,67 +25,83 @@ def main(func, ndata_train=100):
         trainset, batch_size=len(trainset), shuffle=True
     )
 
+    testset = datasets.MNIST(
+        root="./data", train=False, download=True, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset), shuffle=True
+    )
+
     u_i_train, y_train = next(iter(trainloader))
-    y_i_train = torch.nn.functional.one_hot(y_train, num_classes=10).float()
+    u_i_test, y_test = next(iter(testloader))
 
-    u_i_train = u_i_train.numpy()
-    y_i_train = y_i_train.numpy()
+    y_i_train = torch.nn.functional.one_hot(y_train, num_classes=10)
+    y_i_test = torch.nn.functional.one_hot(y_test, num_classes=10)
 
-    # Get 10 PCA components of u_i_train
+    print("Preprocessing data...")
+
     u_i_train_centered = u_i_train - u_i_train.mean(axis=0)
     u_i_train_normalized = u_i_train_centered / (u_i_train_centered.std(axis=0) + 1e-10)
 
-    pca = PCA(n_components=10)
-    u_i_train = pca.fit_transform(u_i_train_normalized)
+    u_i_test_centered = u_i_test - u_i_train.mean(axis=0)
+    u_i_test_normalized = u_i_test_centered / (u_i_train.std(axis=0) + 1e-10)
 
-    u_i_train = u_i_train[:ndata_train, :]
-    y_i_train = y_i_train[:ndata_train, :]
+    # Get 4 PCA components of u_i_train
+    pca = PCA(n_components=4)
+    u_i_train_pca = torch.tensor(pca.fit_transform(u_i_train_normalized))
+    u_i_test_pca = torch.tensor(pca.transform(u_i_test_normalized))
 
-    # Train the springs and sticks model
-    n_pieces = 2 * np.ones_like(u_i_train[0], dtype=int)
-    batch_size, state_size, t_size = 2, np.prod(n_pieces) * 2 * y_i_train.shape[0], 200
-    sde = GGS3DE(
-        n_pieces, u_i_train.T, y_i_train.T, friction=5, temp=1, k=1e-15, M=1e-15
+
+    n_pieces_train = torch.ones_like(u_i_train_pca[0], dtype=int)
+    n_pieces_test = torch.ones_like(u_i_test_pca[0], dtype=int)
+
+    batch_size = 1
+
+    state_size_train = torch.prod(n_pieces_train + 1) * 2 * y_i_train.shape[0]
+    state_size_test = torch.prod(n_pieces_test + 1) * 2 * y_i_test.shape[0]
+
+    sde_train = GGS3DE(
+        n_pieces_train, u_i_train_pca.T, y_i_train.T, friction=FRICTION, temp=TEMP, k=K, M=M
     )
-    ts = torch.linspace(0, 20, t_size)
+    sde_test = GGS3DE(
+        n_pieces_test, u_i_test_pca.T, y_i_test.T, friction=FRICTION, temp=TEMP, k=K, M=M
+    )
 
-    def train():
-        y0 = torch.rand(size=(batch_size, state_size))
-        with torch.no_grad():
-            ys_gen = torchsde.sdeint(
-                sde, y0, ts, method="euler"
-            )  # (t_size, batch_size, state_size)
-        np.save("ys_gen.npy", ys_gen)
+    ts = torch.linspace(0, MAX_TIME, T_SIZE)
+    y0 = torch.rand(size=(batch_size, state_size_train))
 
-    def test():
-        testset = datasets.MNIST(
-            root="./data", train=False, download=True, transform=transform
-        )
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=len(testset), shuffle=True
-        )
+    with torch.no_grad():
+        ys_gen = torchsde.sdeint(sde_train, y0, ts, method="euler")
 
-        u_i_test, y_test = next(iter(testloader))
-        y_i_test = torch.nn.functional.one_hot(y_test, num_classes=10).float()
 
-        ys_gen = np.load("ys_gen.npy")
-        # TODO: get error for testset
+    # Save the output
+    torch.save(ys_gen, "ys_gen.pt")
 
-    if func == "train":
-        train()
-    elif func == "test":
-        test()
-    elif func == "both":
-        train()
-        test()
+    # Calculate the cost
+    train_cost = sde_train.cost(ys_gen[:, 0, :]).detach().numpy()/len(u_i_train)
+    test_cost = sde_test.cost(ys_gen[:, 0, :]).detach().numpy()/len(u_i_test)
 
+    # Save the cost
+    torch.save(train_cost, "train_cost.pt")
+    torch.save(test_cost, "test_cost.pt")
+
+def plot():
+    ys_gen = torch.load("ys_gen.pt")
+    train_cost = torch.load("train_cost.pt")
+    test_cost = torch.load("test_cost.pt")
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(ys_gen[:, 0, :].detach().numpy())
+    plt.title("Generated Data")
+    plt.show()
+
+    plt.plot(train_cost, label="Train Cost")
+    plt.plot(test_cost, label="Test Cost")
+    plt.legend()
+    plt.title("Cost")
+    plt.show()
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--func", choices=["train", "test", "both"])
-    parser.add_argument("--ndata_train", type=int, default=100)
-    args = parser.parse_args()
-
-    main(args.func, args.ndata_train)
+    mnist()
+    plot()
